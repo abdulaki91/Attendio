75b84f652d8f265f7eb5e3010d968a43da908066
abdulaki <abdulakimustefa@gmail.com>
2025-10-15 21:30:24 +0300

    add loading spinner for Card

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,14 +1,19 @@
-import React from "react";
-
-export default function Card({ title, value, icon }) {
+import LoadingSpinner from "../Components/LoadingSpinner";
+export default function Card({ title, value, icon, isLoading }) {
   return (
     <div className="flex items-center justify-center  flex-col w-full md:w-max lg:w-64 p-2 bg-base-300  rounded-lg gap-4 mt-6 text-xs md:text-sm">
       <div className="flex items-center gap-5 px-2">
-        {icon}
-        <div>
-          <p>{title}</p>
-          <p className="font-bold">{value}</p>
-        </div>
+        {isLoading ? (
+          <LoadingSpinner />
+        ) : (
+          <>
+            {icon}
+            <div>
+              <p>{title}</p>
+              <p className="font-bold">{value}</p>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
